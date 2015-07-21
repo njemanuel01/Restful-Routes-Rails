778be605626e8fbfad949cd79f980a7da017b234
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-21 11:42:37 -0500

    Fixed bugs in creation of new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
   end
   
   def new_form
+    @user = User.new
   end
   
   def new_form_do
-    @user = User.new(params["users"])
+    user_params = params.require(:users).permit(:email, :password)
+    
+    @user = User.new(user_params)
     if @user.save
-      redirect "/users/#{@user.id}"
+      redirect_to "/users/#{@user.id}"
+    else
+      render :"users/new_form"
     end
   end
   
