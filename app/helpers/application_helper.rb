baa3226a747ce6d4db48193f97a274768355a96b
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-21 14:21:27 -0500

    Added check_user method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # sets the current user or redirects to the login screen
+  #
+  # returns the current user or redirects
+  def current_user
+    if session[:id]
+      @user = User.find(session[:id].to_i)
+    else
+      session.clear
+      redirect "/"
+    end  
+  end
 end
